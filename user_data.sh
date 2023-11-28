814dfa4e339b0482065d9fb62a8e36ccdaababa0
savak1990 <slava.klovan@gmail.com>
2023-11-28 13:52:36 +0100

    Modified web site text

--- user_data.sh
+++ user_data.sh
@@ -4,6 +4,7 @@ cat > index.html <<EOF
 <h1>Hello, World</h1>
 <p>DB address: ${db_address}</p>
 <p>DB port: ${db_port}</p>
+<h1>Merry Christmas!</h1>
 EOF
 
 nohup busybox httpd -f -p ${server_port} &
