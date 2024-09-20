1a68ca765302c5ae5ad29177777f8939b9bedcc7
savak1990 <slava.klovan@gmail.com>
2024-09-20 15:01:15 +0200

    Updated pointer to current region

--- main.tf
+++ main.tf
@@ -17,10 +17,8 @@ data "aws_subnets" "default" {
   }
 }
 
-data "aws_region" "current" {}
-
 data "aws_ami" "amazon_linux" {
-  provider = aws_region.current
+  provider = aws.current
 
   most_recent = true
   owners      = ["amazon"]
