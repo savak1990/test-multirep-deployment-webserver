f9bfc45a869a1296b160c9851c5c0e809300e3a6
savak1990 <slava.klovan@gmail.com>
2024-09-20 15:10:12 +0200

    Fixed pointers to db

--- main.tf
+++ main.tf
@@ -82,8 +82,8 @@ resource "aws_launch_configuration" "example" {
 
   user_data = templatefile("${path.module}/user_data.sh", {
     server_port = var.server_port
-    db_address  = data.terraform_remote_state.db.outputs.address
-    db_port     = data.terraform_remote_state.db.outputs.port
+    db_address  = data.terraform_remote_state.db.outputs.primary_address
+    db_port     = data.terraform_remote_state.db.outputs.primary_port
     server_text = var.server_text
   })
 
